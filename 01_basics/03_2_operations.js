8d4ca3c9d8e4be5ee9e9ba72ac9f8e8f29446f8e
Akash Padia <35111940+AkashPadia@users.noreply.github.com>
2023-11-24 06:36:48 +0000

    Operations

---
+++ 01_basics/03_2_operations.js
@@ -0,0 +1,28 @@
+let value= 3
+let negValue = -value
+console.log(negValue);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**3);
+// console.log(2/3);
+// console.log(2%3);
+
+let str1 = "Hello"
+let str2 = " World"
+let str3 = str1 + str2
+
+console.log(str3);
+
+console.log("1"+ 2);
+console.log(1 + "2");
+console.log("1" + 2 + 2);
+console.log(1 + 2 + "2");
+
+console.log(+true);
+console.log(+"");
+
+let gameCounter = 100
+gameCounter++;
+console.log(gameCounter);
