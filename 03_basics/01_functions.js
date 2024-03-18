33b2366e655cd57c83222b31727a03faa5f3f5e6
Akash Padia <35111940+AkashPadia@users.noreply.github.com>
2024-03-18 12:47:13 +0000

    functions in javascript

---
+++ 03_basics/01_functions.js
@@ -0,0 +1,26 @@
+function addTwoNumbers(number1, number2) {
+  console.log(number1 + number2);
+}
+
+addTwoNumbers(2, 3);
+
+const result = addTwoNumbers(4, 3);
+console.log("Result:", result);
+
+function addThreeNumbers(number1, number2, number3) {
+  let result = number1 + number2 + number3;
+  return result;
+}
+
+let result1 = addThreeNumbers(1, 2, 3);
+console.log("Result: ", result1);
+
+function loggedInUserMessage(username) {
+  if (!username) {
+    console.log("Please provide username.");
+    return;
+  }
+  return `${username} just logged in.`;
+}
+
+console.log(loggedInUserMessage("Thor"));
